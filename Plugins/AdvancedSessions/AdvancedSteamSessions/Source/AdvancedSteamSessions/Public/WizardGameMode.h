e32e8f19cd91b44951505ac6580dc7aa2149299c
Herbie43 <herblewis43@gmail.com>
2020-02-18 19:08:07 -0600

    A whole HEAP of changes

---
+++ Plugins/AdvancedSessions/AdvancedSteamSessions/Source/AdvancedSteamSessions/Public/WizardGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WizardGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ADVANCEDSTEAMSESSIONS_API AWizardGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
